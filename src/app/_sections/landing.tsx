0e2fb2ea401ed7caa046bef1a51ad2c20577d600
Shubh <shubhkumar357@gmail.com>
2026-06-08 23:29:34 +0530

    Update resume filename to Shubh_Kr_Resume.pdf

--- src/app/_sections/landing.tsx
+++ src/app/_sections/landing.tsx
@@ -128,8 +128,8 @@ const Landing = () => {
                 }}>
                     <Button
                         component="a"
-                        href="/resume/Shubh_Kumar_Resume.pdf"
-                        download="Shubh_Kumar_Resume.pdf"
+                        href="/resume/Shubh_Kr_Resume.pdf"
+                        download="Shubh_Kr_Resume.pdf"
                         size='lg'
                         variant='solid'
                         startDecorator={<DownloadOutlined />}
